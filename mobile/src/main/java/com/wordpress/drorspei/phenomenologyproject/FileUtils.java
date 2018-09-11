e718bdda57a45dda7a6f9a6ba47e354a348b9a8d
drorspei <dror.mastershin@gmail.com>
2018-09-11 20:09:52 +0200

    Changed accessors; FileUtils uses Environment instead of hardcoded

--- mobile/src/main/java/com/wordpress/drorspei/phenomenologyproject/FileUtils.java
+++ mobile/src/main/java/com/wordpress/drorspei/phenomenologyproject/FileUtils.java
@@ -2,6 +2,7 @@ package com.wordpress.drorspei.phenomenologyproject;
 
 import java.io.*;
 
+import android.os.Environment;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -11,44 +12,49 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileUtils {
-    private static final String phenomenaDbPath = "/sdcard/Documents/phenomena.json";
-    private static final String savedPhenomenaDbPath = "/sdcard/Documents/savedphenomena.json";
-
+class FileUtils {
     private static final Type PHENOMENON_TYPE = new TypeToken<ArrayList<Phenomenon>>() {}.getType();
     private static final Type SAVED_PHENOMENON_TYPE = new TypeToken<ArrayList<SavedPhenomenon>>() {}.getType();
 
-    public static ArrayList<Phenomenon> loadPhenomena() {
+    private static String getPhenomenaDbPath() {
+        return Environment.getExternalStorageDirectory().getPath() + "/Documents/phenomena.json";
+    }
+
+    private static String getSavedPhenomenaDbPath() {
+        return Environment.getExternalStorageDirectory().getPath() + "/Documents/savedphenomena.json";
+    }
+
+    static ArrayList<Phenomenon> loadPhenomena() {
         Gson gson = new Gson();
 
         try {
-            JsonReader reader = new JsonReader(new FileReader(phenomenaDbPath));
+            JsonReader reader = new JsonReader(new FileReader(getPhenomenaDbPath()));
             return gson.fromJson(reader, PHENOMENON_TYPE);
         } catch (FileNotFoundException e) {
             return new ArrayList<>();
         }
     }
 
-    public static ArrayList<SavedPhenomenon> loadSavedPhenomena() {
+    static ArrayList<SavedPhenomenon> loadSavedPhenomena() {
         Gson gson = new Gson();
 
         try {
-            JsonReader reader = new JsonReader(new FileReader(savedPhenomenaDbPath));
+            JsonReader reader = new JsonReader(new FileReader(getSavedPhenomenaDbPath()));
             return gson.fromJson(reader, SAVED_PHENOMENON_TYPE);
         } catch (FileNotFoundException e) {
             return new ArrayList<>();
         }
     }
 
-    public static void savePhenomena(List<Phenomenon> phenomena) throws IOException{
-        try (Writer writer = new FileWriter(phenomenaDbPath)) {
+    static void savePhenomena(List<Phenomenon> phenomena) throws IOException{
+        try (Writer writer = new FileWriter(getPhenomenaDbPath())) {
             Gson gson = new GsonBuilder().create();
             gson.toJson(phenomena, writer);
         }
     }
 
-    public static void saveSavedPhenomena(List<SavedPhenomenon> savedPhenomena) throws IOException{
-        try (Writer writer = new FileWriter(savedPhenomenaDbPath)) {
+    static void saveSavedPhenomena(List<SavedPhenomenon> savedPhenomena) throws IOException{
+        try (Writer writer = new FileWriter(getSavedPhenomenaDbPath())) {
             Gson gson = new GsonBuilder().create();
             gson.toJson(savedPhenomena, writer);
         }
